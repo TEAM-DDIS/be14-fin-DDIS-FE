f21a636f1370a92d5767cb22d63d500c5347769f
1214alex <jerry20011214@gmail.com>
2025-05-27 11:20:07 +0900

    no message

--- src/router/index.js
+++ src/router/index.js
@@ -21,11 +21,11 @@ const router = createRouter({
         },
         {
             path: '/org/evaluation',
-            component: () => import('@/component/EvaluationPage.vue')
+            component: () => import('@/components/Evaluation/EvaluationPage.vue')
         },
         {
             path: '/org/goals',
-            component: () => import('@/component/GoalListPage.vue')
+            component: () => import('@/components/Goals/GoalListPage.vue')
         }
 
     ]
