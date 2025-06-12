4738a53969d0958bc0a1ff9adbeeb2dc97eaa7b1
RKDLDE <kangcanddy@gmail.com>
2025-06-12 11:24:29 +0900

    [feat] 팀 근태 현황 박스

--- src/components/home/AttendanceBox.vue
+++ src/components/home/AttendanceBox.vue
@@ -1,7 +1,13 @@
-<!-- 팀 근태 현황 박스 -->
 <template>
-  <div class="box attendance">팀 근태 현황</div>
+  <div class="box attendance">
+    팀 근태 현황
+    <TeamStatusCard />
+  </div>
 </template>
+
+<script setup>
+import TeamStatusCard from '@/components/attendance/TeamWorkStatusCard.vue' 
+</script>
 
 <style scoped>
 .attendance {
@@ -9,6 +15,7 @@
   min-height: 420px;
 }
 .box {
+  margin-top: 30px;
   background: #fff;
   border-radius: 15px;
   padding: 20px;
@@ -16,5 +23,24 @@
   box-shadow: 1px 1px 20px 1px rgba(0, 0, 0, 0.05);
   font-size: 17px;
   font-weight: bold;
+}
+::v-deep(.team-status-card) {
+  box-shadow: none !important;
+  border-radius: 12px !important;
+  padding: 0px !important;
+  /* min-height: 420px !important; */
+}
+::v-deep(.team-title) {
+  margin-left:3px;
+  font-size: 16px;
+  /* color: #333; */
+  margin-bottom: 5px;
+}
+::v-deep(.status-list) {
+  gap: -4px;
+}
+/* 리스트 아이템 간 간격 줄이기 */
+::v-deep(.status-list > * + *) {
+  margin-top: 4px;
 }
 </style>
