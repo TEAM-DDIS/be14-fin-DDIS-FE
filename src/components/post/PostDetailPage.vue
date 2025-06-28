186a844baea4e5fb705aa02581cd305399503286
RKDLDE <kangcanddy@gmail.com>
2025-06-28 19:48:45 +0900

    [style] 공지사항 순서 다크모드 적용

--- src/components/post/PostDetailPage.vue
+++ src/components/post/PostDetailPage.vue
@@ -477,7 +477,7 @@ onMounted(fetchNotice)
   box-sizing: border-box;
 }
 .btn-save:hover {
-  background-color: #fff;
+  background-color: var(--bg-main);
   color: var(--primary);
   border-color: var(--primary);
   box-shadow: inset 1px 1px 6px rgba(0, 0, 0, 0.15);
@@ -499,7 +499,7 @@ onMounted(fetchNotice)
   color: #fff;
 }
 .btn-cancel {
-    background-color: #d3d3d3;
+  background-color: #d3d3d3;
   color: #000;
   border: none;
   border-radius: 10px;
@@ -541,18 +541,19 @@ onMounted(fetchNotice)
   border-radius: 12px;
   padding: 20px 32px;
   box-shadow: 0 4px 12px rgba(0, 0, 0, 0.05);
-  margin: 24px 10px;
+    margin-left: 20px;
+  margin-top: 10px;
 }
 
 /* form-row 공통 */
 .form-row {
   display: flex;
   align-items: center;
-  margin-bottom: 24px;
+  margin-bottom: 20px;
 }
 .form-label {
   width: 100px;
-  margin-right: 12px;
+  /* margin-right: 12px; */
   font-weight: 500;
 }
 .required {
@@ -563,19 +564,21 @@ onMounted(fetchNotice)
 .form-input {
   flex: 1;
   padding: 12px 16px;
-  border: 1px solid #ccc;
+  border: 1px solid var(--border-color);
   border-radius: 8px;
   font-size: 15px;
   box-sizing: border-box;
+  background-color: var(--bg-main);
 }
-.form-input:hover {
-  border-color: #1f2937;
+.form-input:disabled {
+  color: var(--text-sub); /* 원하는 색으로 */
+  opacity: 1; /* 회색 흐림 방지 */
 }
-.form-input:focus {
-  outline: none;
-  border-color: #1f2937;
+.form-input:enabled {
+  background: var(--bg-label-cell);
+  color: var(--text-main);
+  opacity: 1; /* 회색 흐림 방지 */
 }
-
 /* 파일 첨부 드롭존 */
 .file-row {
   margin-bottom: 8px;
@@ -584,23 +587,23 @@ onMounted(fetchNotice)
   flex: 1;
   position: relative;
   height: 50px;
-  border: 1px solid #c8c8c8;
+  border: 1px solid var(--border-color);
   border-radius: 8px;
   display: flex;
   align-items: center;
   justify-content: space-between;
   padding: 12px 16px;
   box-sizing: border-box;
+  background-color: var(--bg-main);
+
   cursor: pointer;
-  transition: border-color 0.2s, background-color 0.2s;
 }
-.file-dropzone:hover {
-  border-color: #1f2937;
-  background-color: #f9fcff;
+.file-dropzone:hover{
+  color: var(--primary);
 }
 .file-dropzone p {
   margin: 0;
-  color: #888;
+  /* color: #888; */
   font-size: 14px;
 }
 .file-buttons {
@@ -620,7 +623,7 @@ onMounted(fetchNotice)
   box-shadow: 0 1px 4px rgba(0, 0, 0, 0.05);
 }
 .btn-save.small:hover {
-  background-color: #fff;
+  /* background-color: #fff; */
   color: var(--primary);
   border-color: var(--primary);
   box-shadow: inset 1px 1px 6px rgba(0, 0, 0, 0.15);
@@ -633,8 +636,9 @@ onMounted(fetchNotice)
 
 /* 첨부된 파일 목록 */
 .attached-list {
+  background-color: var(--bg-main);
   list-style: none;
-  margin: 10px 0 0 112px; /* 레이블 100px + 간격 12px */
+  margin: 10px 0 0 100px; /* 레이블 100px + 간격 12px */
   padding: 0;
 }
 .file-item {
@@ -642,11 +646,11 @@ onMounted(fetchNotice)
   align-items: center;
   justify-content: space-between;
   padding: 6px 12px;
-  border: 1px solid #eee;
-  border-radius: 6px;
+  border: 1px solid var(--border-color);
+  border-radius: 8px;
   margin-bottom: 8px;
   font-size: 14px;
-  color: #444;
+  /* color: #444; */
 }
 .file-name {
   overflow: hidden;
@@ -659,7 +663,7 @@ onMounted(fetchNotice)
 .instructions {
   margin-top: 10px;
   font-size: 12px;
-  color: #666;
+  color: var(--text-sub);
   line-height: 1.4;
   margin-left: 112px; /* 레이블 100px + 간격 12px */
 }
@@ -675,7 +679,8 @@ onMounted(fetchNotice)
 }
 .editor {
   min-height: 300px;
-  border: 1px solid #ccc;
+  background: var(--bg-main);
+  border: 1px solid var(--border-color);
   border-radius: 8px;
   padding: 12px 16px;
   font-size: 15px;
@@ -683,12 +688,10 @@ onMounted(fetchNotice)
   box-sizing: border-box;
   position: relative;
 }
-.editor:hover {
-  border-color: #1f2937;
-}
+
 .editor:focus {
   outline: none;
-  border: 1px solid #1f2937;
+  border: 1px solid var(--text-main);
 }
 .editor.empty:before {
   content: attr(placeholder);
